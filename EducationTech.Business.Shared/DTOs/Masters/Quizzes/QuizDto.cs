6b17c0cdf0ab16ff7df61442a5ebc132d27fd490
ThinhTranCSE <thinh.tk1110@gmail.com>
2024-11-15 18:15:24 +0700

    Update QuizDto.cs

--- EducationTech.Business.Shared/DTOs/Masters/Quizzes/QuizDto.cs
+++ EducationTech.Business.Shared/DTOs/Masters/Quizzes/QuizDto.cs
@@ -7,7 +7,7 @@ namespace EducationTech.Business.Shared.DTOs.Masters.Quizzes
     public class QuizDto : AbstractDto<Quiz, QuizDto>
     {
         public int Id { get; set; }
-        public int LessonId { get; set; }
+        public int LearningObjectId { get; set; }
         public int TimeLimit { get; set; }
         public ICollection<QuestionDto> Questions { get; set; } = new List<QuestionDto>();
     }
