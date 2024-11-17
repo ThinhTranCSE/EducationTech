c26c880258e9bdd4adc19fe03038ec9c26bc82aa
ThinhTranCSE <thinh.tk1110@gmail.com>
2024-11-17 21:20:06 +0700

    Update CourseService.cs

--- EducationTech.Business/Master/CourseService.cs
+++ EducationTech.Business/Master/CourseService.cs
@@ -107,8 +107,8 @@ namespace EducationTech.Business.Master
         {
             var query = _unitOfWork.Courses.GetAll()
                 .Include(c => c.Owner)
-                .Include(c => c.Specialities)
                 .Include(c => c.Prerequisites)
+                .Include(c => c.Specialities)
                     .ThenInclude(cs => cs.Speciality)
                 .Include(c => c.CourseGroup)
                 .Include(c => c.Topics)
