a560b0c24fc92b706997b51661d83e6a775a6a75
ThinhTranCSE <thinh.tk1110@gmail.com>
2024-11-19 19:40:09 +0700

    remove neo4j

--- EducationTech/Installers/Installer_Neo4j.cs
+++ EducationTech/Installers/Installer_Neo4j.cs
@@ -1,28 +1,24 @@
-﻿
-using EducationTech.Configurations;
-using Neo4jClient;
-
-namespace EducationTech.Installers;
+﻿namespace EducationTech.Installers;
 
 public class Installer_Neo4j : IInstaller
 {
     public IServiceCollection InstallServicesToServiceCollection(IServiceCollection services, IConfiguration configuration)
     {
-        var neo4jConfiguration = configuration.GetSection("Neo4j").Get<Neo4jConfiguration>();
-        services.AddSingleton<IGraphClient>(provider =>
-        {
-            var graphClient = new BoltGraphClient(new Uri(neo4jConfiguration.Uri), neo4jConfiguration.Username, neo4jConfiguration.Password);
-            var connectTask = graphClient.ConnectAsync();
-            connectTask.Wait();
-            return graphClient;
-        });
+        //var neo4jConfiguration = configuration.GetSection("Neo4j").Get<Neo4jConfiguration>();
+        //services.AddSingleton<IGraphClient>(provider =>
+        //{
+        //    var graphClient = new BoltGraphClient(new Uri(neo4jConfiguration.Uri), neo4jConfiguration.Username, neo4jConfiguration.Password);
+        //    var connectTask = graphClient.ConnectAsync();
+        //    connectTask.Wait();
+        //    return graphClient;
+        //});
 
-        services.AddSingleton<NeoServerConfiguration>(context =>
-        {
-            return NeoServerConfiguration.GetConfigurationAsync(new Uri(neo4jConfiguration.Uri), neo4jConfiguration.Username, neo4jConfiguration.Password).GetAwaiter().GetResult();
-        });
+        //services.AddSingleton<NeoServerConfiguration>(context =>
+        //{
+        //    return NeoServerConfiguration.GetConfigurationAsync(new Uri(neo4jConfiguration.Uri), neo4jConfiguration.Username, neo4jConfiguration.Password).GetAwaiter().GetResult();
+        //});
 
-        services.AddSingleton<IGraphClientFactory, GraphClientFactory>();
+        //services.AddSingleton<IGraphClientFactory, GraphClientFactory>();
 
 
         return services;
